c5dd7a2eb4bcbb9f88c33ce9fa17b0bbdc763e3f
Roth <croth3@luc.edu>
2024-10-09 13:16:22 -0500

    try 8

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World 2.6!!!\n";
+        return "Hello World 2.8!!!\n";
         
         //yo
     }
